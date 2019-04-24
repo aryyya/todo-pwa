5daacf1a9fb29610108f09afec1f4c176beea2d3
aryyya <aryyya.xyz@gmail.com>
2019-04-24 12:55:59 -0700

    Listen for install and activate service worker events.

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,18 @@
 workbox.skipWaiting()
 workbox.clientsClaim()
 
+self.addEventListener('install', event => {
+  console.log('install event fired')
+
+  const asyncInstall = new Promise(resolve => {
+    resolve()
+  })
+
+  event.waitUntil(asyncInstall)
+})
+
+self.addEventListener('activate', event => {
+  console.log('activate event fired')
+})
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
