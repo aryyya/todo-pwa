687e989716644ee27e5807f04b3e21001d26a426
aryyya <aryyya.xyz@gmail.com>
2019-04-24 13:03:34 -0700

    Cache static cdn assets and api responses!

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,20 @@
 workbox.skipWaiting()
 workbox.clientsClaim()
 
+workbox.routing.registerRoute(
+  new RegExp('https:.*min\.(css|js)'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'cdn-cache'
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('http://.*:4567.*\.json'),
+  workbox.strategies.networkFirst({
+    cacheName: 'api-cache'
+  })
+)
+
 self.addEventListener('install', event => {
   console.log('install event fired')
 
